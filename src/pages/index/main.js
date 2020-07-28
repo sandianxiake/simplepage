5aec2daf19b2d4ad325ec401b8111a6715a0e662
sandianxiake <sandianxiake@163.com>
2020-07-28 23:37:14 +0800

    初始化文件

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,6 +1,5 @@
+import Vue from 'vue';
 import "../../common/base.less";
 import "../../config";
 import App from "./App.vue";
-import kwview from '../../components';
-Vue.use(kwview);
 new Vue(App).$mount('#root');
